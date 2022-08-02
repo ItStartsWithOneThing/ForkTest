552e103e5cabfdef6b94b6f42c8dee29f4ee6bb4
Konstantin Turkin <like69.kt@gmail.com>
2022-08-02 17:59:25 +0300

    Added spiderman

---
+++ ForkTest/dto/Spiderman.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForkTest.dto
+{
+    public class Spiderman
+    {
+        public string Power { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string SecondPower { get; set; }
+        public string CoupleName { get; set; }
+        public string EnemyName { get; set; }
+    }
+}
